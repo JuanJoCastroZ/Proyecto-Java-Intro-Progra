46b2648d167ffbbc9d6362824ff081f161b75575
JuanJoCastroZ <80114387+JuanJoCastroZ@users.noreply.github.com>
2024-09-26 22:46:23 -0600

    First commit

---
+++ Proyecto_queso/src/main/java/com/mycompany/proyecto_queso/Proyecto_queso.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_queso;
+
+/**
+ *
+ * @author Juan Jose
+ */
+public class Proyecto_queso {
+    public int sumar(int a, int b){
+        return a +b;
+    }
+    
+    public static void main(String[] args){
+        Proyecto_queso pq = new Proyecto_queso();
+    }
+}
